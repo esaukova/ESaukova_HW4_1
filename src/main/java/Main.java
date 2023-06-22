42be4a9f97023809a84f753b3e86a2e9247189f1
Ekaterina Saukova <esaukova@outlook.com>
2023-06-22 18:16:20 +0300

    BonusService / Mavin / Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+
+        long expected = 30;
+        long actual = service.calculate(1000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 500;
+        actual = service.calculate (1_000_000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
+    }
+
+
+}
